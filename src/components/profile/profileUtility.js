cf46b70abcb3593f55baf69d170bad0b74eb5d5a
Swaminaraya Sharma <sharmaswami.ss@gmail.com>
2023-04-05 11:11:34 +0530

    updated profileUtil

--- src/components/profile/profileUtility.js
+++ src/components/profile/profileUtility.js
@@ -6,26 +6,22 @@ import { useState } from 'react';
 function isEmptyOrSpaces(str) {
     return str === null || str.match(/^ *$/) !== null;
 }
-function validation(emailValue, userValue, passwordValue) {
-    let emailCheck = /^[a-zA-Z0-9]+@(gmail|yahoo|outlook)\.com$/;
+function validation( userValue, userNumber) {
+    // let emailCheck = /^[a-zA-Z0-9]+@(gmail|yahoo|outlook)\.com$/;
     // let emailCheck = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
     let userCheck = /^[a-zA-Z0-9]{5,}$/;
 
-    if (isEmptyOrSpaces(emailValue) || isEmptyOrSpaces(userValue)) {
+    if ( isEmptyOrSpaces(userValue)) {
         alert("You cannot left any field empty");
         return false;
     }
 
-    if (!emailCheck.test(emailValue)) {
-        alert("enter a valid email");
-        return false;
-    }
 
     if (!userCheck.test(userValue)) {
         alert("-username can only be alphanumeric \n -usernamemust be atleast 5 characters")
         return false;
     }
-    if (passwordValue.length < 10) {
+    if (userNumber.length < 10) {
         alert("please Enter the correct number");
         return false;
     }
@@ -37,24 +33,22 @@ function validation(emailValue, userValue, passwordValue) {
 
 function Solve(e) {
     e.preventDefault();
-    //  const data = new FormData(e.target);
     const data = new FormData(e.target);
     const userName = data.get('profileName');
-    const userEmail = data.get('profileEmail');
+    // const userEmail = data.get('profileEmail');
     const userNumber = data.get('profileNumber');
     const userGender = data.get('profileGender');
 
 
 
-    console.log(userEmail, userName, userNumber);
+    // console.log(userEmail, userName, userNumber);
 
-    if (!validation(userEmail, userName, userNumber)) {
+    if (!validation( userName, userNumber)) {
 
     } else {
 
         updateProfile(auth.currentUser, {
             displayName: userName,
-            email: userEmail,
         }).then(() => {
             alert("You have successfully update your profile")
         }).catch((error) => {
